1f4110ab42bb67ba2c672b4bc6c61cadf284211f
Gavaskar821996 <gavaskar821996@gmail.com>
2016-06-06 21:41:47 +0530

    Update diifmonth.py

--- diifmonth.py
+++ diifmonth.py
@@ -1,1 +1,6 @@
-return (d1.year - d2.year)*12 + d1.month - d2.month
+from datetime import datetime
+def days_between(d1, d2):
+    d1 = datetime.strptime(d1, "%Y-%m-%d")
+    d2 = datetime.strptime(d2, "%Y-%m-%d")
+    return (d1.year - d2.year)*12 + d1.month - d2.month
+print days_between('2012-10-20','2012-05-25')
