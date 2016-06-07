a7ce1536a755b93a150abd6462dfb6f82672bd02
Gavaskar821996 <gavaskar821996@gmail.com>
2016-06-07 09:30:58 +0530

    Create diffdate2.py

---
+++ diffdate2.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+def days_between(d1, d2):
+    d1 = datetime.strptime(d1, "%Y-%m-%d")
+    d2 = datetime.strptime(d2, "%Y-%m-%d")
+    return abs((d2 - d1).days)
+print days_between('2012-05-20','2012-05-25')
